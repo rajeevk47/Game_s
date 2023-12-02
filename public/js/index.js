550743f934f542373107e0846a75da836872bcb5
rajeevk47 <rajeevkumawat2007@gmail.com>
2023-12-02 12:33:53 +0530

    done

--- public/js/index.js
+++ public/js/index.js
@@ -13,14 +13,31 @@ const y = canvas.height / 2
 
 const player = new Player(x, y, 10, 'white')
 
+socket.on('updatePlayers', (backendplayers)=>{
+  
+  for(const id in backendplayers){
+    const backendplayer = backendplayers[id]
+    if(!player[id]){
+      players[id] = new Player(backendplayer.x , backendplayer.y,10,'white')
+    }
+  }
+  console.log(players)
+})
+
 let animationId
 
 function animate() {
   animationId = requestAnimationFrame(animate)
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
+  
+  for (const id in players){
+    const player = player[id]
+    player
+  }
 
-  player.draw()}
+
+}
 
  
 
