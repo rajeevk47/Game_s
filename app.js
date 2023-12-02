3fd3d1ca3e285c3d4f1df19c133666d83e799162
rajeevk47 <rajeevkumawat2007@gmail.com>
2023-12-02 11:15:07 +0530

    Just commited

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+app.listen(port, () =>{
   console.log(`Example app listening on port ${port}`)
 })
